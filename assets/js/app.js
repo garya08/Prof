b23dcd461e3d71023b0e27a97d5f71e63ec0b099
garya08 <garyaev_ar10@mail.ru>
2024-06-17 22:46:20 +0300

    jj

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,7 @@ const fs = require('fs');
 
 // read json file
 
-const data = fs.readFileSync('./test.json', {encoding: 'utf-8'});   // чтение JSON файла
+const data = fs.readFileSync('/assets/server/db.json', {encoding: 'utf-8'});   // чтение JSON файла
 // console.log(data);
-let dataObj = JSON.parse(data);     // парсинг объекта
-
-document.querySelector('.userbloc').innerHTML = `<section class="section_user></section>`
-
-let tex = document.createElement('p').innerHTML = `<span>${dataObj.username}</span><br><span>${dataObj.adress}</span><br><span>${dataObj.tel}</span>`;
-document.querySelector('.userbloc').appendChild(tex);
+var dataObj = JSON.parse(data);     // парсинг объекта
+console.log(dataObj);
